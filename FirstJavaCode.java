5614ef2a816d878872e4e6adbc589337bad8a7bb
Sourabh Patil <81312909+SourabhGPatil@users.noreply.github.com>
2022-04-01 10:28:44 +0530

    Updated the code with evenOdd function

--- FirstJavaCode.java
+++ FirstJavaCode.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 class FirstJavaCode
 {
 
@@ -7,12 +8,26 @@ class FirstJavaCode
           System.out.println("Hello to Java Programming World");  
           FirstJavaCode fjc = new FirstJavaCode();
                  fjc.show();
-           fjc.otherMethod();    
+                 fjc.otherMethod();
+                 fjc.evenOdd();
           }
          
          public void show(){
   System.out.println("Second Code Hello!!");
 }
+         
+         public static void evenOdd()
+         {
+            Scanner reader = new Scanner(System.in);
+
+            System.out.print("Enter a number: ");
+            int num = reader.nextInt();
+
+            if(num % 2 == 0)
+                System.out.println(num + " is even");
+            else
+                System.out.println(num + " is odd");
+        }
               
           
          
