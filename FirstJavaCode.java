5105c9b709b4408abfb4e20696770c4e9aa98667
Manobala-N <85210746+Manobala-N@users.noreply.github.com>
2022-04-01 10:22:03 +0530

    Updated Sample method

--- FirstJavaCode.java
+++ FirstJavaCode.java
@@ -3,9 +3,14 @@ class FirstJavaCode
 
          public static void main(String...args)
          {
-
+          FirstJavaCode cm=new FirstJavaCode();
+          cm.sampleMethod();
           System.out.println("Hello to Java Programming World");  
 
           }
+         
+         void sampleMethod(){
+          System.out.println("Hello");          
+         }
        
 }
