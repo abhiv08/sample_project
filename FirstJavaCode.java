cbfe723b2bb8bd9c48d62fd8bc8120fb9098aa15
sasmithakarthi <85392596+sasmithakarthi@users.noreply.github.com>
2022-04-01 10:14:23 +0530

    updated FirstJavaCode

--- FirstJavaCode.java
+++ FirstJavaCode.java
@@ -4,8 +4,14 @@ class FirstJavaCode
          public static void main(String...args)
          {
 
-          System.out.println("Hello to Java Programming World");  
+          System.out.println("Hello to Java Programming World"); 
+                  FirstJavaCode s=new FirstJavaCode();
+                  s.newMethod();
 
           }
+         void newMethod()
+         {
+                  System.out.println("Hello its new method");
+         }
        
 }
