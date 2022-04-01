0aaeaa7834cd914b8032e5c0c7820f311e7cb07e
AkhilP63039 <97536601+AkhilP63039@users.noreply.github.com>
2022-04-01 10:31:09 +0530

    update from akhil

--- FirstJavaCode.java
+++ FirstJavaCode.java
@@ -7,7 +7,8 @@ class FirstJavaCode
           System.out.println("Hello to Java Programming World");  
           FirstJavaCode fjc = new FirstJavaCode();
                  fjc.show();
-           fjc.otherMethod();    
+           fjc.otherMethod(); 
+                 fjc.newMethod();
           }
          
          public void show(){
@@ -21,6 +22,10 @@ class FirstJavaCode
           System.out.println("Hello its otherMethod from thinkbot");  
                     
                   
+         }
+         void newMethod()
+         {
+                  System.out.println("Hello this is a new small change from akhil");
          }
        
 }
