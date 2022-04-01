64dbd980312e2208d621b3010bc8a18b0bc1613d
abhiv08 <102590606+abhiv08@users.noreply.github.com>
2022-04-01 09:40:47 +0530

    uploaded FirstJavaCode
    it contains a public static void main(String...args) method

---
+++ FirstJavaCode.java
@@ -0,0 +1,11 @@
+class FirstJavaCode
+{
+
+         public static void main(String...args)
+         {
+
+          System.out.println("Hello to Java Programming World");  
+
+          }
+       
+}
