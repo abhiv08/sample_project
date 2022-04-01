7ec1adebf9fbfca5f5e50226d95ac124b1a61753
Ana Josephine <95203885+annajosephine503@users.noreply.github.com>
2022-04-01 10:14:06 +0530

    Updated FirstJavaCode

--- FirstJavaCode.java
+++ FirstJavaCode.java
@@ -5,7 +5,12 @@ class FirstJavaCode
          {
 
           System.out.println("Hello to Java Programming World");  
-
+          FirstJavaCode fjc = new FirstJavaCode();
+                  fjc.show();
           }
+         
+         public void show(){
+  System.out.println("Second Code Hello!!");
+}
        
 }
